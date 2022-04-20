4fe14882137c9a28ac0abda353b9494e3c2f711c
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2022-04-20 17:37:28 +0300

    Add page 404

--- 404.php
+++ 404.php
@@ -2,8 +2,19 @@
 
 get_header();
 ?>
-
-
-
+<section class="breadcrumbs">
+  <div class="wrapper">
+    <?php if( function_exists('kama_breadcrumbs') ) kama_breadcrumbs(''); ?>
+  </div>
+</section>
+<section class="error-page">
+	<div class="wrapper">
+		<div class="error-page__inner">
+			<h1 class="error-page__title">404</h1>
+			<h2 class="error-page__subtitle">Ой! Эта страница не может быть найдена.</h2>
+			<a class="error-page__link" href="https://edelweys.milkers.by/">Перейти на главную</a>
+		</div>
+	</div>
+</section>
 <?php
 get_footer();
