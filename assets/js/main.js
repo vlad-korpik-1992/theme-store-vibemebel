315b4c97d2023586773623fb3b4ee5bf638eae82
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2022-04-20 15:29:21 +0300

    Add responsive breakpoint to cart__sliderprev

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,51 @@ $(document).ready(function() {
     asNavFor: '.cart__slider',
     dots: false,
     arrows: false,
-    focusOnSelect: true
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 5
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 6
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 5
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          dots: false,
+          slidesToShow: 4,
+          centerMode: false
+        }
+      },
+      {
+        breakpoint: 424,
+        settings: {
+          dots: false,
+          slidesToShow: 3,
+          centerMode: false
+        }
+      },
+      {
+        breakpoint: 374,
+        settings: {
+          dots: false,
+          slidesToShow: 2,
+          centerMode: false
+        }
+      }
+    ]
   });
 
   /* scrollto */
